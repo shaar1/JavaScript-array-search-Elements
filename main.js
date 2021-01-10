0f02ba9de138f9ae17ec73777353c879bacf64d1
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 14:55:08 +0100

    träning om hur vi kan visa en namn som är röd

--- main.js
+++ main.js
@@ -15,6 +15,28 @@ var specialFriends = friends.indexOf("Ali");
 
 console.log(specialFriends);
 */
+/*
+var friends = [
+    "Hassan",
+    "Hosam",
+    "Mohamad",
+    "Mahmoud",
+    "Ali",
+    "Yara",
+    "Toni",
+    "Antoanit"
+];
+
+var specialFriends = friends.indexOf("Ali");
+
+console.log(specialFriends);
+
+console.log(friends[specialFriends]);
+
+document.getElementById("all").innerHTML = 
+
+" My Friends Are: " + friends;
+*/
 
 var friends = [
     "Hassan",
@@ -36,3 +58,7 @@ console.log(friends[specialFriends]);
 document.getElementById("all").innerHTML = 
 
 " My Friends Are: " + friends;
+
+document.getElementById("special").innerHTML = 
+
+" My Special Friends Is: " + "<span style='color:#F00'>" + friends[specialFriends] + "</span> ;"
