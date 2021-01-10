7b214b1c438bf1e5ff9793e8b14a35c65d4cd280
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 14:47:14 +0100

    träning om indexof i array

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 /* global console, alert, prompt*/
+/*
+var friends = [
+    "Hassan",
+    "Hosam",
+    "Mohamad",
+    "Mahmoud",
+    "Ali",
+    "Yara",
+    "Toni",
+    "Antoanit"
+];
+
+var specialFriends = friends.indexOf("Ali");
+
+console.log(specialFriends);
+*/
 
 var friends = [
     "Hassan",
@@ -14,3 +30,9 @@ var friends = [
 var specialFriends = friends.indexOf("Ali");
 
 console.log(specialFriends);
+
+console.log(friends[specialFriends]);
+
+document.getElementById("all").innerHTML = 
+
+" My Friends Are: " + friends;
