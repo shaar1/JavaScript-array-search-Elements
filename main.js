2b48bbe9755081d4eb0c38eef554269691a009e4
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 15:17:13 +0100

    träning om lastIndexOf och söker om införmationen som finns i array

--- main.js
+++ main.js
@@ -173,7 +173,7 @@ document.getElementById("special").innerHTML =
 
 " My Special Friends Is: " + "<span style='color:#F00'>" + friends[specialFriends] + "</span> ;"
 */
-
+/*
 var friends = [
     "Hassan",
     "Hosam",
@@ -199,3 +199,140 @@ document.getElementById("special").innerHTML =
 
 " My Special Friends Is: " + "<span style='color:#F00'>" + friends[specialFriends] + "</span> ;"
 
+*/
+/*
+var friends = [
+    "Hassan",   // 0 index
+    "Hosam",    // 1 index
+    "Mohamad",  // 2 index
+    "Mahmoud",   // 3 index
+    "Ali",       // 4 index
+    "Yara",      // 5 index
+    "Toni",      // 6 index
+    "Antoanit"   // 7 index
+];
+
+// lastIndexOf("Value To Search", String Index)
+
+var specialFriends = friends.lastIndexOf("Yara"); 
+
+console.log(specialFriends);
+
+console.log(friends[specialFriends]);
+
+document.getElementById("all").innerHTML = 
+
+" My Friends Are: " + friends;
+
+document.getElementById("special").innerHTML = 
+
+" My Special Friends Is: " + "<span style='color:#F00'>" + friends[specialFriends] + "</span> ;"
+
+
+*/
+
+/*
+var friends = [
+        "Hassan",
+        "Hosam",
+        "Mohamad",
+        "Mahmoud",
+        "Ali",
+        "Yara",
+        "Toni",
+        "Antoanit"
+];
+        var specialFriends = friends.indexOf("Yara", 3);
+
+        console.log(specialFriends);
+
+        console.log(friends[specialFriends]);
+
+        document.getElementById("all").innerHTML =
+
+        " My Friends Are: " + friends;
+
+        document.getElementById("special").innerHTML =
+
+        " My Special Friends Is: " + "<span style='color:#F00'>" + friends[specialFriends] + "</span> ;"
+       
+        */
+/*
+       var friends = [
+        "Hassan",
+        "Hosam",
+        "Mohamad",
+        "Mahmoud",
+        "Ali",
+        "Yara",
+        "Toni",
+        "Antoanit"
+];
+        var specialFriends = friends.lastIndexOf("Yara", 3);
+
+        console.log(specialFriends);
+
+        console.log(friends[specialFriends]);
+
+        document.getElementById("all").innerHTML =
+
+        " My Friends Are: " + friends;
+
+        document.getElementById("special").innerHTML =
+
+        " My Special Friends Is: " + "<span style='color:#F00'>" + friends[specialFriends] + "</span> ;"
+      
+        */
+/*
+var friends = [
+    "Yara",
+    "Hassan",
+    "Hosam",
+    "Mohamad",
+    "Mahmoud",
+    "Ali",
+    "Yara",
+    "Toni",
+    "Antoanit"
+];
+var specialFriends = friends.lastIndexOf("Yara", 3);
+
+console.log(specialFriends);
+
+console.log(friends[specialFriends]);
+
+document.getElementById("all").innerHTML =
+
+    " My Friends Are: " + friends;
+
+document.getElementById("special").innerHTML =
+
+    " My Special Friends Is: " + "<span style='color:#F00'>" + friends[specialFriends] + "</span> ;"
+
+    */
+
+var friends = [
+    "Ommar",
+    "Yara",
+    "Hassan",
+    "Hosam",
+    "Mohamad",
+    "Mahmoud",
+    "Ali",
+    "Yara",
+    "Toni",
+    "Antoanit"
+];
+var specialFriends = friends.lastIndexOf("Yara", 3);
+
+console.log(specialFriends);
+
+console.log(friends[specialFriends]);
+
+document.getElementById("all").innerHTML =
+
+    " My Friends Are: " + friends;
+
+document.getElementById("special").innerHTML =
+
+    " My Special Friends Is: " + "<span style='color:#F00'>" + friends[specialFriends] + "</span> ;"
